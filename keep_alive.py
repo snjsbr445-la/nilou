9450de3ac7d3c58373e368322d184ebe42754648
Rohan <snjsbr445@gmail.com>
2025-07-23 01:29:58 +0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import subprocess
 
 app = Flask('')
 
@@ -8,8 +9,17 @@ def home():
     return "I'm alive"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
+# main.py ফাইল অটো চালু করা হচ্ছে
+def run_main():
+    subprocess.Popen(["python", "main.py"])
+
+# Flask চালু + main.py চালু
+if __name__ == "__main__":
+    keep_alive()
+    run_main()
